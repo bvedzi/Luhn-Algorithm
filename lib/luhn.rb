f5a1e7e762bc9692fddae4048a611e9f1f619989
Brian Vedzi <vdzvsk@gmail.com>
2019-12-02 18:08:18 +0000

    added to_s on line 4

--- lib/luhn.rb
+++ lib/luhn.rb
@@ -1,7 +1,7 @@
 module Luhn
   def self.is_valid?(number)
     #WRITE YOUR SOLUTION HERE
-  digits = number.chars.map(&:to_i)
+  digits = number.to_s.chars.map(&:to_i)
   check = digits.pop
 
   sum = digits.reverse.each_slice(2).flat_map do |x, y|
@@ -9,5 +9,8 @@ module Luhn
   end.flatten.inject(:+)
 
   check.zero? ? sum % 10 == 0 : (10 - sum % 10) == check
-end
   end
+end
+
+puts "#{Luhn.is_valid?(4194560385008504)}"
+puts "#{Luhn.is_valid?(4194560385008505)}"
